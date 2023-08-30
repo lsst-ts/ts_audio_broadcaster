73dad01f2415b1d52143e85ca8eb1acd230fbfd2
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2023-08-30 11:05:30 -0400

    Configurations adjustments

--- doc/conf.py
+++ doc/conf.py
@@ -1,14 +1,14 @@
 """Sphinx configuration file for an LSST stack package.
 
 This configuration only affects single-package Sphinx documentation builds.
-For more information, see:
-https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.conf.pipelinespkg import *
-
+import lsst.ts.salobj  # noqa
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 
 project = "ts_audio_broadcaster"
-html_theme_options["logotext"] = project
+html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
+# Avoid warning: Could not find tag file _doxygen/doxygen.tag
+doxylink = {}  # type: ignore
